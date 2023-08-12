978a71681e7d592bd5911ec43049313e7f13c1e9
dd86k <dd@dax.moe>
2023-08-11 23:12:14 -0400

    disasm: Add package

---
+++ src/adbg/v2/disassembler/package.d
@@ -0,0 +1,8 @@
+/// Disassembler package.
+///
+/// Authors: dd86k <dd@dax.moe>
+/// Copyright: © dd86k <dd@dax.moe>
+/// License: BSD-3-Clause
+module adbg.v2.disassembler;
+
+public import adbg.v2.disassembler.core;
