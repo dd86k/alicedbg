fc9273daffe32bc09cdff12ab7b42e786a5db387
dd86k <dd@dax.moe>
2023-09-16 13:22:35 -0400

    Add root and v2 package modules

---
+++ src/adbg/package.d
@@ -0,0 +1,8 @@
+/// Package for whole library.
+///
+/// Authors: dd86k <dd@dax.moe>
+/// Copyright: © dd86k <dd@dax.moe>
+/// License: BSD-3-Clause
+module adbg;
+
+public import adbg.v2, adbg.error;
