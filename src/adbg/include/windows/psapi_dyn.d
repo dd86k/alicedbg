8341c32e9b08caac550a4c3821bcf14b9c1d6646
dd86k <dd@dax.moe>
2023-09-22 10:56:22 -0400

    include: Simplify psapi_dyn module

--- src/adbg/include/windows/psapi_dyn.d
+++ src/adbg/include/windows/psapi_dyn.d
@@ -1,108 +1,21 @@
 /// Dynamic definitions for psapi.
 ///
-/// Copied from core.sys.windows.psapi.
+/// Authors: dd86k <dd@dax.moe>
+/// Copyright: © dd86k <dd@dax.moe>
+/// License: BSD-3-Clause
 module adbg.include.windows.psapi_dyn;
 
 version (Windows):
 
 version (ANSI) {} else version = Unicode;
 
-import core.sys.windows.w32api;
-import core.sys.windows.winbase;
 import core.sys.windows.windef;
+public import core.sys.windows.psapi;
 
 import bindbc.loader;
 import bindbc.loader.sharedlib;
 
 extern (Windows):
-
-struct MODULEINFO {
-    LPVOID lpBaseOfDll;
-    DWORD SizeOfImage;
-    LPVOID EntryPoint;
-}
-alias MODULEINFO* LPMODULEINFO;
-
-struct PSAPI_WS_WATCH_INFORMATION {
-    LPVOID FaultingPc;
-    LPVOID FaultingVa;
-}
-alias PSAPI_WS_WATCH_INFORMATION* PPSAPI_WS_WATCH_INFORMATION;
-
-struct PSAPI_WS_WATCH_INFORMATION_EX {
-    PSAPI_WS_WATCH_INFORMATION BasicInfo;
-    ULONG_PTR FaultingThreadId;
-    ULONG_PTR Flags;
-}
-alias PSAPI_WS_WATCH_INFORMATION_EX* PPSAPI_WS_WATCH_INFORMATION_EX;
-
-struct PROCESS_MEMORY_COUNTERS {
-    DWORD cb;
-    DWORD PageFaultCount;
-    SIZE_T PeakWorkingSetSize;
-    SIZE_T WorkingSetSize;
-    SIZE_T QuotaPeakPagedPoolUsage;
-    SIZE_T QuotaPagedPoolUsage;
-    SIZE_T QuotaPeakNonPagedPoolUsage;
-    SIZE_T QuotaNonPagedPoolUsage;
-    SIZE_T PagefileUsage;
-    SIZE_T PeakPagefileUsage;
-}
-alias PROCESS_MEMORY_COUNTERS* PPROCESS_MEMORY_COUNTERS;
-
-struct PERFORMANCE_INFORMATION {
-    DWORD cb;
-    SIZE_T CommitTotal;
-    SIZE_T CommitLimit;
-    SIZE_T CommitPeak;
-    SIZE_T PhysicalTotal;
-    SIZE_T PhysicalAvailable;
-    SIZE_T SystemCache;
-    SIZE_T KernelTotal;
-    SIZE_T KernelPaged;
-    SIZE_T KernelNonpaged;
-    SIZE_T PageSize;
-    DWORD HandleCount;
-    DWORD ProcessCount;
-    DWORD ThreadCount;
-}
-alias PERFORMANCE_INFORMATION* PPERFORMANCE_INFORMATION;
-
-struct ENUM_PAGE_FILE_INFORMATION {
-    DWORD cb;
-    DWORD Reserved;
-    SIZE_T TotalSize;
-    SIZE_T TotalInUse;
-    SIZE_T PeakUsage;
-}
-alias ENUM_PAGE_FILE_INFORMATION* PENUM_PAGE_FILE_INFORMATION;
-
-/* application-defined callback function used with the EnumPageFiles()
- * http://windowssdk.msdn.microsoft.com/library/ms682627.aspx */
-alias BOOL function(LPVOID, PENUM_PAGE_FILE_INFORMATION, LPCWSTR)
-    PENUM_PAGE_FILE_CALLBACKW;
-alias BOOL function(LPVOID, PENUM_PAGE_FILE_INFORMATION, LPCSTR)
-    PENUM_PAGE_FILE_CALLBACKA;
-
-version (Unicode) {
-    alias PENUM_PAGE_FILE_CALLBACKW PENUM_PAGE_FILE_CALLBACK;
-    alias GetModuleBaseNameW GetModuleBaseName;
-    alias GetModuleFileNameExW GetModuleFileNameEx;
-    alias GetMappedFileNameW GetMappedFileName;
-    alias GetDeviceDriverBaseNameW GetDeviceDriverBaseName;
-    alias GetDeviceDriverFileNameW GetDeviceDriverFileName;
-    alias EnumPageFilesW EnumPageFiles;
-    alias GetProcessImageFileNameW GetProcessImageFileName;
-} else {
-    alias PENUM_PAGE_FILE_CALLBACKA PENUM_PAGE_FILE_CALLBACK;
-    alias GetModuleBaseNameA GetModuleBaseName;
-    alias GetModuleFileNameExA GetModuleFileNameEx;
-    alias GetMappedFileNameA GetMappedFileName;
-    alias GetDeviceDriverBaseNameA GetDeviceDriverBaseName;
-    alias GetDeviceDriverFileNameA GetDeviceDriverFileName;
-    alias EnumPageFilesA EnumPageFiles;
-    alias GetProcessImageFileNameA GetProcessImageFileName;
-}
 
 private {
     alias pEnumProcesses = BOOL function(DWORD*, DWORD, DWORD*);
@@ -132,7 +45,24 @@ private {
     alias pGetPerformanceInfo = BOOL function(PPERFORMANCE_INFORMATION, DWORD);
     alias pEnumPageFilesW = BOOL function(PENUM_PAGE_FILE_CALLBACKW, LPVOID);
     alias pEnumPageFilesA = BOOL function(PENUM_PAGE_FILE_CALLBACKA, LPVOID);
-    
+}
+
+version (ANSI) {
+    alias GetProcessImageFileName = GetProcessImageFileNameA;
+    alias GetModuleBaseName = GetModuleBaseNameA;
+    alias GetModuleFileNameEx = GetModuleFileNameExA;
+    alias GetDeviceDriverBaseName = GetDeviceDriverBaseNameA;
+    alias GetDeviceDriverFileName = GetDeviceDriverFileNameA;
+    alias GetMappedFileName = GetMappedFileNameA;
+    alias EnumPageFiles = EnumPageFilesA;
+} else {
+    alias GetProcessImageFileName = GetProcessImageFileNameW;
+    alias GetModuleBaseName = GetModuleBaseNameW;
+    alias GetModuleFileNameEx = GetModuleFileNameExW;
+    alias GetDeviceDriverBaseName = GetDeviceDriverBaseNameW;
+    alias GetDeviceDriverFileName = GetDeviceDriverFileNameW;
+    alias GetMappedFileName = GetMappedFileNameW;
+    alias EnumPageFiles = EnumPageFilesW;
 }
 
 __gshared {
@@ -204,6 +134,5 @@ bool __dynlib_psapi_load() {
     bindSymbol(lib, cast(void**)&EnumPageFilesA, "EnumPageFilesA");
     
     __dynlib_psapi_loaded = errors.length == 0;
-    
-    return errors.length > 0;
+    return !__dynlib_psapi_loaded;
 }
