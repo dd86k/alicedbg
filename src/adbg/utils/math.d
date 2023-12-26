67fc8fe2aad9242c96e1c08e3a7ef6f5f5577962
dd86k <dd@dax.moe>
2023-12-26 16:32:59 -0500

    math: Add KiB/MiB/GiB templates

--- src/adbg/utils/math.d
+++ src/adbg/utils/math.d
@@ -5,9 +5,38 @@
 /// License: BSD-3-Clause
 module adbg.utils.math;
 
+/// Choose the highest number.
 template MAX(size_t a, size_t b) {
 	enum MAX = a >= b ? a : b;
 }
+/// Choose the lowest number.
 template MIN(size_t a, size_t b) {
 	enum MIN = a <= b ? a : b;
+}
+
+@system unittest {
+	static assert(MAX!(1, 2) == 2);
+	static assert(MIN!(1, 2) == 1);
+}
+
+/// Make a constant binary size (base 1024^3).
+template GiB(int a) {
+	enum ulong GiB = a * 1024L * 1024L * 1024L;
+}
+/// Make a constant binary size (base 1024^2).
+template MiB(int a) {
+	enum ulong MiB = a * 1024L * 1024L;
+}
+/// Make a constant binary size (base 1024^1).
+template KiB(int a) {
+	enum ulong KiB = a * 1024L;
+}
+
+@system unittest {
+	static assert(KiB!1 == 1024);
+	static assert(KiB!2 == 1024 * 2);
+	static assert(MiB!1 == 1024 * 1024);
+	static assert(MiB!3 == 1024 * 1024 * 3);
+	static assert(GiB!1 == 1024 * 1024 * 1024);
+	static assert(GiB!4 == 1024L * 1024 * 1024 * 4);
 }
